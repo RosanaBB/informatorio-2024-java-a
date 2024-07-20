ce9f7accd1f555cd39274262d3ae2968246cad89
RosanaBB <rosanabaez_01@hotmail.com>
2024-07-20 20:11:21 -0300

    01-info/imperativa - conversiones

---
+++ imperativa/conversiones/Conversiones.java
@@ -0,0 +1,19 @@
+package imperativa.conversiones;
+
+public class Conversiones {
+    public static void main(String[] args) {
+
+        //Conversion de tipo promocion
+        int value = 5;
+
+        //Conversion de tipo contraccion
+        int value2 = (int)5.69f; //Cast
+        System.out.println(value2);
+
+        //Conversion de tipo implicita
+        double value3 = 10 * 20.5d;
+        System.out.println(10 * 20.5d);
+
+        
+    }
+}
