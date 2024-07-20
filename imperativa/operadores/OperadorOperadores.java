fee58be55a0f4e39eba966d88cb33b1c4bf8dd42
RosanaBB <rosanabaez_01@hotmail.com>
2024-07-20 20:02:30 -0300

    01-info/imperativa - operadores

---
+++ imperativa/operadores/OperadorOperadores.java
@@ -0,0 +1,22 @@
+package imperativa.operadores;
+
+public class OperadorOperadores {
+    public static void main(String[] args) {
+        int value2;
+        int value = 5;
+
+        int resultado = 5 + 3 * 2;
+
+        int resultado2 = 20/4 * 2 + 3;
+
+        //Operador ternario
+        // < <= > >=
+        boolean resultado3 = (resultado2 > resultado)? true : false;
+
+        int value = 5;
+        value2 = value++; //asigna luego incrementa
+        value2 = ++value; //incrementa valua luego asigna
+        value2 = --value; //decrementa valua luego asigna
+
+    }
+}
