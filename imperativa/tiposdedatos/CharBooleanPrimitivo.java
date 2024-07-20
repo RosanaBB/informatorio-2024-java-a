e83caec23b191e2be635ac4151c433963fd55397
RosanaBB <rosanabaez_01@hotmail.com>
2024-07-20 18:58:10 -0300

    01-info/programacion-imperativa - tipos de datos

---
+++ imperativa/tiposdedatos/CharBooleanPrimitivo.java
@@ -0,0 +1,18 @@
+package imperativa.tiposdedatos;
+
+public class CharBooleanPrimitivo {
+    public static void main(String[] args) {
+
+        //Char es caracter a b c d e f
+        //Los caracteres siempre van encerrados entre '  '
+
+        char unChar = 'a';
+        char unCharConUnicode = '\u0044';
+        System.out.println("unChar = " + unChar);
+
+        //tipos booleanos
+        boolean miVerdadero = true;
+        boolean miFalso = false;
+        
+    }
+}
---
+++ imperativa/tiposdedatos/FloatDoublePrimitivo.java
@@ -0,0 +1,21 @@
+package imperativa.tiposdedatos;
+
+import com.sun.jdi.Value;
+
+public class FloatDoublePrimitivo {
+    public static void main(String[] args) {
+
+        //Float tiene 32 bits
+        float miMinimoValorDeFloat = Float.MIN_VALUE;
+        float miMaximoValorDeFloat = 3.40f;
+
+        //double tiene 64 bits
+        double miMinimoValorDeDouble = Double.MIN_VALUE;
+        double miMaximoValorDeDouble = Double.MAX_VALUE;
+
+        double valorConFloat = 3.40f;
+
+
+
+    }
+}
